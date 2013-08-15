bf4176a51170e69651bf057e6db09dad5a36e904
Hugh Cayless <philomousos@gmail.com>
2013-08-15 07:05:18 +0200

    Continuing reorg.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject dll "0.1.0-SNAPSHOT"
+(defproject tei-xpointer.js "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [compojure "1.1.5"]
                  [ring/ring-core "1.2.0-beta1"]]
   :plugins [[lein-ring "0.8.2"]]
-  :ring {:handler dll.handler/app}
+  :ring {:handler tei-xpointer.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}}
   :resources-path "resources")
