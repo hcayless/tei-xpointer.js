1bb29eeaeaa96cc31584b364a48f252893bd0db6
Hugh Cayless <philomousos@gmail.com>
2013-08-16 14:26:56 -0400

    Added enclojure webservice files.

---
+++ src/tei_xpointer/handler.clj
@@ -0,0 +1,22 @@
+(ns tei_xpointer.handler
+  (:use [compojure.core]
+        [tei_xpointer.views])
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [compojure.response :as response]
+            [ring.util.response :as resp]))
+
+(defroutes app-routes
+  (GET "/" [] (splash-page))
+  (GET "/proxy/:uri"
+    [uri] 
+    (bp-uri-response uri))
+  (GET "/cts/dll/:author.:work/:section" 
+    [author work section] (bp-file-response (str author "_" work "." section ".1-13.xml") {:root "public"}))
+  (GET "/cts/dll/:author.:work/:section/xml" 
+    [author work section] (resp/resource-response (str author "_" work "." section ".1-13.xml") {:root "public"}))
+  (route/resources "/")
+  (route/not-found "No."))
+
+(def app
+  (-> (handler/site app-routes)))
